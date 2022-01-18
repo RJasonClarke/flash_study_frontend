3b0b796a04b459ea5a61f5926b6a063cec27b31a
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2022-01-18 13:01:20 -0600

    Changed link text and form inputs reset on submit

--- flash_study_frontend/src/App.js
+++ flash_study_frontend/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
         <h1 className='site-title'>Flash-Study</h1>
         <nav className="nav-bar">
           <Link to="/">Home</Link>
-          <Link to="/cards">Cards</Link>
+          <Link to="/cards">All Cards</Link>
           <Link to="/cards/new">New Card</Link>
           <Link to="/study">Study</Link>
           <Link to="/about">About</Link>
--- flash_study_frontend/src/components/CardsForm.js
+++ flash_study_frontend/src/components/CardsForm.js
@@ -19,18 +19,19 @@ class CardsForm extends Component {
     handleSubmit = (e) => {
         e.preventDefault()
         this.props.addCard(this.state)
+        document.getElementById("cardForm").reset()
     }
  
     render() {
         return (
             <div>
                 <h2>Create A New Card: </h2>
-               <form onSubmit={this.handleSubmit}>
+               <form id="cardForm" onSubmit={this.handleSubmit}>
                     <label>Question: </label>
-                    <input type="text" value={this.state.name} onChange={this.handleChange} name="question"></input>
+                    <input type="text" required value={this.state.name} onChange={this.handleChange} name="question"></input>
                     <br/>  
                     <label>Answer: </label>
-                    <input type="text" value={this.state.name} onChange={this.handleChange} name="answer"></input>
+                    <input type="text" required value={this.state.name} onChange={this.handleChange} name="answer"></input>
                     <br/>
                     <input type="submit" value="Save Card"></input>
                 </form> 
