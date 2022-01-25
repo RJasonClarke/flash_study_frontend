e7328bfdd0bfe67adec7c852b4ff68195c039d17
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2022-01-25 11:56:07 -0600

    notes on schwartzian transfom

--- flash_study_frontend/src/components/CardStudy.js
+++ flash_study_frontend/src/components/CardStudy.js
@@ -12,9 +12,9 @@ const CardStudy = ({cards}) => {
         <div>
             <h2>Click a card below to see the answer:</h2>
                 <ul onClick={handleClick}>
-                    {cards.map((card) => ({ card, sort: Math.random() }))
-                    .sort((a, b) => a.sort - b.sort)
-                    .map(({ card }) => 
+                    {cards.map((card) => ({ card, sort: Math.random() })) //assign each card a random sort key
+                    .sort((a, b) => a.sort - b.sort) //sort using random key
+                    .map(({ card }) => //unmap to get original objects
                         <li id={card.id} key={card.id} className='card'>
                             <div id='question'>{card.question}</div><br/>
                             <div id='answer'>{card.answer}</div>
