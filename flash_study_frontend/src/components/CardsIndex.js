68c931e869fd6cebea0dca4cfb3821570e6c3ae8
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2022-08-23 10:24:48 -0500

    added styling to cards display

--- flash_study_frontend/src/components/CardsIndex.js
+++ flash_study_frontend/src/components/CardsIndex.js
@@ -5,7 +5,7 @@ const CardsIndex = ({cards}) => {
     return(
         <div>
             <h2>Your Flash Cards:</h2>
-            <ul>
+            <ul className="cards-index">
             {cards.map(card => 
                     <li key={card.id} className='card'>
                         {card.question} <br/> <br/>
